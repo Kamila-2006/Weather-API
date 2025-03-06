1dfbfa3f2e0246cf5ae2d4a893a8d8537c6e98f1
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-03-06 20:10:00 +0500

    locations created

--- forecasts/models.py
+++ forecasts/models.py
@@ -4,11 +4,11 @@ from locations.models import Location
 
 class Forecast(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='forecast')
-    forecast_date = models.DateField(auto_now_add=True)
+    forecast_date = models.DateField()
     temperature_min = models.FloatField()
     temperature_max = models.FloatField()
     humidity = models.FloatField()
     precipitation_probability = models.FloatField()
     wind_speed = models.FloatField()
     wind_direction = models.FloatField()
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
