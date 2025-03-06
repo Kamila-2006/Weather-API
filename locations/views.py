aee38aaa69662687d69375993bbfc79ffeaff98f
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-03-06 22:29:07 +0500

    locations created

--- locations/views.py
+++ locations/views.py
@@ -1,11 +1,13 @@
 from rest_framework import generics
 from .models import Location
 from .serializers import LocationSerializer
+from .pagination import LocationListPagination
 
 
 class LocationListCreateView(generics.ListCreateAPIView):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
+    pagination_class = LocationListPagination
 
 class LocationDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Location.objects.all()
