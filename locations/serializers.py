1c375d3a839ceda4c2d5a9093c18d060a2dcc5cc
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-03-06 22:31:56 +0500

    locations created

--- locations/serializers.py
+++ locations/serializers.py
@@ -7,3 +7,7 @@ class LocationSerializer(serializers.ModelSerializer):
         model = Location
         fields = ['id', 'name', 'latitude', 'longitude', 'elevation', 'created_at']
         read_only_fields = ['id', 'created_at']
+
+class LocationShortSerializer(serializers.ModelSerializer):
+    model = Location
+    fields = ['id', 'name']
