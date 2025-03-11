66459ed6011b25e2fa9411d13b3755096b537e67
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-03-11 19:55:42 +0500

    analitycs created

--- weatherdata/urls.py
+++ weatherdata/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('weather-data/', views.WeatherDataListCreateView.as_view(), name='weather-data-list'),
     path('weather-data/<int:pk>/', views.WeatherDataDetailView.as_view(), name='weather-data-detail'),
     path('weather-data/location/<int:location_id>/', views.LocationWeatherDataView.as_view(), name='location-weather-data'),
+    path('analytics/temperature-avg/', views.AverageTemperatureView.as_view(), name='average-temperature'),
+    path('analytics/precipitation-sum/', views.TotalPrecipitationView.as_view(), name='total-precipitation'),
+    path('analytics/wind-speed-max/', views.MaxWindSpeedView.as_view(), name='max-wind-speed'),
 ]
--- weatherdata/views.py
+++ weatherdata/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+from django.db.models import Avg, Sum, Max
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import WeatherData
 from .serializers import WeatherDataSerializer
 from .pagination import WeatherDataPagination
@@ -20,3 +23,18 @@ class LocationWeatherDataView(generics.ListAPIView):
     def get_queryset(self):
         location_id = self.kwargs['location_id']
         return WeatherData.objects.filter(location_id=location_id)
+
+class AverageTemperatureView(APIView):
+    def get(self, request):
+        avg_temp = WeatherData.objects.aggregate(average_temperature=Avg('temperature'))
+        return Response(avg_temp)
+
+class TotalPrecipitationView(APIView):
+    def get(self, request):
+        total_precip = WeatherData.objects.aggregate(total_precipitation=Sum('precipitation'))
+        return Response(total_precip)
+
+class MaxWindSpeedView(APIView):
+    def get(self, request):
+        max_wind_speed = WeatherData.objects.aggregate(max_wind_speed=Max('wind_speed'))
+        return Response(max_wind_speed)
